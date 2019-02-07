6e0f17744ab18e5d64ac435f5b6738d7b03a80f7
nppalani <nagendra.palani@gmail.com>
2019-02-07 11:53:58 -0600

    Update umitag.py
    Script edited to process modified GUIDE-seq protocol.

--- umitag.py
+++ umitag.py
@@ -6,18 +6,10 @@ import itertools
 import argparse
 import subprocess
 
-__author__ = 'Martin Aryee'
+__author__ = 'Original - Martin Aryee; Modified by Nagendra Palani (UMGC)'
 
-# python ~/work/projects/umi/umitag.py --read1_in bcl/Undetermined_S0_L001_R1_001.fastq.gz --read2_in bcl/Undetermined_S0_L001_R2_001.fastq.gz --read1_out umi1.fastq.gz --read2_out umi2.fastq.gz --index1 bcl/Undetermined_S0_L001_I1_001.fastq.gz --index2 bcl/Undetermined_S0_L001_I2_001.fastq.gz
-
-# args = {'out_dir':'/PHShome/ma695/tmp', 'min_reads':10}
-# base = '/data/joung/sequencing_bcl/131007_M01326_0075_000000000-A6B33/Data/Intensities/BaseCalls'
-# args['read1_in'] = os.path.join(base, 'Undetermined_S0_L001_R1_001.fastq.gz')
-# args['read2_in'] = os.path.join(base, 'Undetermined_S0_L001_R2_001.fastq.gz')
-# args['read1_out'] = os.path.join(out_dir, 'Undetermined_S0_L001_R1_001.umi.fastq.gz')
-# args['read2_out'] = os.path.join(out_dir, 'Undetermined_S0_L001_R2_001.umi.fastq.gz')
-# args['index1'] = os.path.join(base, 'Undetermined_S0_L001_I1_001.fastq.gz')
-# args['index2'] = os.path.join(base, 'Undetermined_S0_L001_I2_001.fastq.gz')
+# WARNING: This script works ONLY for libraries generated 
+# using the protocol at https://dx.doi.org/10.17504/protocols.io.wikfccw
 
 def fq(file):
     if re.search('.gz$', file):
@@ -36,11 +28,21 @@ def fq(file):
 
 
 # Create molecular ID by concatenating molecular barcode and beginning of r1 and r2 read sequences
-def get_umi(r1, r2, i1, i2):
-    molecular_barcode = i2[1][8:16]
-    return '%s_%s_%s' % (molecular_barcode, r1[1][0:6], r2[1][0:6])
+def get_umi(r1, r2):
+    molecular_barcode = r2[1][0:8]
+    # extract the first 8 bases of Read2, which is the UMI
 
-def umitag(read1, read2, index1, index2, read1_out, read2_out, out_dir):
+    return '%s_%s_%s' % (molecular_barcode, r1[1][35:41], r2[1][40:46])
+    # Extract 6 bases from Read1 after the primer region.
+    # Extract 6 bases from Read2 after the primer region. 
+    # Note that I arbitrarily picked a window of base positions that will contain the genomic sequence to generate the UMI barcode. 
+    # (which is 8 bp of molecular barcode + 6 bp of R1 genomic sequence + 6 bp of R2 genomic sequence)
+    # You can change the window value within this function to suit your library design (longer/shorter UMI),
+    # different R1 & R2 genomic sequence window lengths etc. If you change R2 window value, change the adapter trim window in 
+    # ADAPTERTRIM section appropriately.
+
+    
+def umitag(read1, read2, read1_out, read2_out, out_dir):
 
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
@@ -51,14 +53,21 @@ def umitag(read1, read2, index1, index2, read1_out, read2_out, out_dir):
     # Create UMI-tagged R1 and R2 FASTQs
     r1_umitagged = open(r1_umitagged_unsorted_file, 'w')
     r2_umitagged = open(r2_umitagged_unsorted_file, 'w')
-    #it = itertools.izip(fq(args['read1_in']), fq(args['read2_in']), fq(args['index1']), fq(args['index2']))
-    #for r1,r2,i1,i2 in itertools.islice(it, 0, 100):
-    for r1,r2,i1,i2 in itertools.izip(fq(read1), fq(read2), fq(index1), fq(index2)):
-        # Create molecular ID by concatenating molecular barcode and beginning of r1 read sequence
-        molecular_id = get_umi(r1, r2, i1, i2)
-        # Add molecular id to read headers
+
+    # Because demultiplexed libraries will be run through this script, removed references to index files. Only needs R1 & R2 fastq for each sample.
+    for r1,r2 in itertools.izip(fq(read1), fq(read2)):
+        
+        molecular_id = get_umi(r1, r2)
+        # Remove default fastq read headers from fastq and add molecular id to read headers
         r1[0] = '%s %s\n' % (r1[0].rstrip(), molecular_id)
         r2[0] = '%s %s\n' % (r2[0].rstrip(), molecular_id)
+
+        # ADAPTERTRIM
+        r2[1] = '%s' % (r2[1][47:])
+        r2[3] = '%s' % (r2[3][47:])
+        # Chop the 5' adapter sequence from R2 
+    
+
         for line in r1:
             r1_umitagged.write(line)
         for line in r2:
@@ -83,13 +92,14 @@ def main():
     parser.add_argument('--read2_in', required=True)
     parser.add_argument('--read1_out', required=True)
     parser.add_argument('--read2_out', required=True)
-    parser.add_argument('--index1', required=True)
-    parser.add_argument('--index2', required=True)
     parser.add_argument('--out_dir', default='.')
     args = vars(parser.parse_args())
     out_dir = args['out_dir']
 
-    umitag(read1_in, read2_in, index1, index2, read1_out, read2_out, out_dir)
+    umitag(args['read1_in'], args['read2_in'], args['read1_out'], args['read2_out'], args['out_dir'])
+    # Because demultiplexed libraries will be run through this script, removed references to index files. Only needs R1 & R2 fastq for each sample.
+    # fixed argument syntax based on reported issue.
+
 
 if __name__ == '__main__':
     main()
